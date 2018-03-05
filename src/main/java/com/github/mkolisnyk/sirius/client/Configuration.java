e52017ee516c746bc915374514fd0eac669aa293
mkolisnyk <kolesnik.nickolay@gmail.com>
2018-03-05 15:04:11 +0000

    PMD error fixes

--- src/main/java/com/github/mkolisnyk/sirius/client/Configuration.java
+++ src/main/java/com/github/mkolisnyk/sirius/client/Configuration.java
@@ -6,9 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
 import java.util.Map.Entry;
 import java.util.Properties;
 
